775b207d6a20ec715f41eb2eda23469bd61145d7
Tony Zhao <41414030+tawnyzhao@users.noreply.github.com>
2020-01-14 15:50:55 -0500

    add polarity vhdl

---
+++ Lab1/output_files/POLARITY_gates.vhd
@@ -0,0 +1,20 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+LIBRARY work;
+
+ENTITY POLARITY_gates IS
+	PORT (
+		IN_1, IN_2, IN_3, IN_4, SW: IN STD_LOGIC;
+		OUT_1, OUT_2, OUT_3, OUT_4: OUT STD_LOGIC
+		);
+END POLARITY_gates;
+		
+ARCHITECTURE simple_gates OF POLARITY_gates IS
+BEGIN
+
+OUT_1 <= IN_1 XOR SW
+OUT_2 <= IN_2 XOR SW
+OUT_3 <= IN_3 XOR SW
+OUT_4 <= IN_4 XOR SW
+
+END simple_gates;
